20ae7c41351828b4f07eaff1ec1a51bb261e8207
ngochai020899 <43904044+ngochai020899@users.noreply.github.com>
2018-11-04 16:18:16 +0700

    test

--- src/view/MainJFrame.java
+++ src/view/MainJFrame.java
@@ -17,7 +17,7 @@ public class MainJFrame extends javax.swing.JFrame {
     public MainJFrame() {
         initComponents();        
     }
-âdfgh
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
